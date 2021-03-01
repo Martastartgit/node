4ec6b0bb36be79c5534e3a93d44c74dbe64f4a69
Martastartgit <marta.petrivska@gmail.com>
2021-03-01 12:03:11 +0200

    lesson5

--- lesson4/router/api.router.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const residentRouter = require('./resident.router');
-const apartmentRouter = require('./apartment.router');
-
-router.use('/residents', residentRouter);
-router.use('/apartments', apartmentRouter);
-
-module.exports = router;
